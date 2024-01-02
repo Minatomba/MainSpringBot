614080d4422d8c8bf2d1b9f7c6631ab49b6a2a79
Jelizaveta Kruglova <114922000+anjufehno@users.noreply.github.com>
2024-01-02 18:22:35 +0200

    Initial commit

---
+++ dispatcher/src/main/java/io/project/service/AnswerConsumer.java
@@ -0,0 +1,7 @@
+package io.project.service;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public interface AnswerConsumer {
+    void consume(SendMessage sendMessage);
+}
