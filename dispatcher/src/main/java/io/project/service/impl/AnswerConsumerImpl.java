eb938f6c7bcf18027eb9d5835b373a2524f315dc
Minatomba <109984683+Minatomba@users.noreply.github.com>
2024-01-21 23:16:47 +0200

    Node module was added

---
+++ dispatcher/src/main/java/io/project/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,24 @@
+package io.project.service.impl;
+
+import io.project.controller.UpdateController;
+import io.project.service.AnswerConsumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import static io.project.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl  implements AnswerConsumer {
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
---
+++ node/src/main/java/io/project/configuration/RabbitConfiguration.java
@@ -0,0 +1,17 @@
+package io.project.configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static io.project.model.RabbitQueue.*;
+
+@Configuration
+public class RabbitConfiguration {
+    @Bean
+     public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+}
